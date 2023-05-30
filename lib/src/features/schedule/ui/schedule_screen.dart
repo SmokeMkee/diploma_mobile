d5cae49533364977e1d7876795fa5ac0462263d7
Buitekb <bbayan@inbox.ru>
2023-05-30 22:00:25 +0600

    schedule screen

--- lib/src/features/schedule/ui/schedule_screen.dart
+++ lib/src/features/schedule/ui/schedule_screen.dart
@@ -19,60 +19,68 @@ class ScheduleScreen extends StatelessWidget {
           ),
           backgroundColor: AppColors.white,
           centerTitle: true,
-          title: Column(
-            children: [
-              const Text(
-                'Schedule',
-                style: AppStyles.s15w600,
-              ),
-              Text(
-                DateFormat.MMMMEEEEd('en').format(DateTime.now()),
-                style: AppStyles.s11w400,
-              )
-            ],
-          ),
-        ),
-        body: Column(
-          children: [
-            const SizedBox(height: 12),
-            const TabBar(
-              isScrollable: true,
-              indicatorColor: AppColors.accent,
-              indicatorSize: TabBarIndicatorSize.label,
-              labelColor: AppColors.primary,
-              labelStyle: AppStyles.s17w500,
-              unselectedLabelColor: AppColors.gray600,
-              tabs: [
-                Text('Mon'),
-                Text('Tue'),
-                Text('Wed'),
-                Text('Thur'),
-                Text('Fri'),
-                Text('Sat'),
-                Text('Sun'),
+          title: Semantics(
+            explicitChildNodes: true,
+            enabled: true,
+            child: Column(
+              children: [
+                const Text(
+                  'Schedule',
+                  style: AppStyles.s15w600,
+                ),
+                Text(
+                  DateFormat.MMMMEEEEd('en').format(DateTime.now()),
+                  style: AppStyles.s11w400,
+                )
               ],
             ),
-            Container(
-              height: 1,
-              width: double.infinity,
-              color: AppColors.gray200,
-            ),
-            Flexible(
-              child: TabBarView(
-                children: [
-                  Expanded(
-                    child: ListView.builder(
-                      itemBuilder: (context, int index) {
-                        return const ScheduleCard();
-                      },
-                      shrinkWrap: true,
-                      itemCount: 3,
-                    ),
-                  ),
+          ),
+        ),
+        body: Semantics(
+          explicitChildNodes: true,
+          enabled: true,
+          child: Column(
+            children: [
+              const SizedBox(height: 12),
+              const TabBar(
+                isScrollable: true,
+                indicatorColor: AppColors.accent,
+                indicatorSize: TabBarIndicatorSize.label,
+                labelColor: AppColors.primary,
+                labelStyle: AppStyles.s17w500,
+                unselectedLabelColor: AppColors.gray600,
+                tabs: [
+                  Text('Mon'),
+                  Text('Tue'),
+                  Text('Wed'),
+                  Text('Thur'),
+                  Text('Fri'),
+                  Text('Sat'),
+                  Text('Sun'),
                 ],
               ),
-            ),
-          ],
+              Container(
+                height: 1,
+                width: double.infinity,
+                color: AppColors.gray200,
+              ),
+              Flexible(
+                child: TabBarView(
+                  children: [
+                    Expanded(
+                      child: ListView.builder(
+                        itemBuilder: (context, int index) {
+                          return const ScheduleCard();
+                        },
+                        shrinkWrap: true,
+                        itemCount: 3,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
@@ -94,35 +102,43 @@ class ScheduleCard extends StatelessWidget {
         margin: const EdgeInsets.symmetric(vertical: 6),
         child: Padding(
           padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 15),
-          child: Row(
-            children: [
-              const CourseContainer(),
-              const SizedBox(width: 12),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    '09:00 - 09:30',
-                    style: AppStyles.s14w500.copyWith(
-                      fontSize: 12,
-                    ),
+          child: Semantics(
+            explicitChildNodes: true,
+            enabled: true,
+            child: Row(
+              children: [
+                const CourseContainer(),
+                const SizedBox(width: 12),
+                Semantics(
+                  explicitChildNodes: true,
+                  enabled: true,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        '09:00 - 09:30',
+                        style: AppStyles.s14w500.copyWith(
+                          fontSize: 12,
+                        ),
+                      ),
+                      Text(
+                        'General English',
+                        style: AppStyles.s14w500.copyWith(),
+                      ),
+                      Text(
+                        'Zoom',
+                        style: AppStyles.s11w400.copyWith(
+                          color: AppColors.subtitle,
+                        ),
+                      )
+                    ],
                   ),
-                  Text(
-                    'General English',
-                    style: AppStyles.s14w500.copyWith(),
-                  ),
-                  Text(
-                    'Zoom',
-                    style: AppStyles.s11w400.copyWith(
-                      color: AppColors.subtitle,
-                    ),
-                  )
-                ],
-              ),
-              const Spacer(),
-              const Text('Lecture')
-            ],
+                ),
+                const Spacer(),
+                const Text('Lecture')
+              ],
+            ),
           ),
         ),
       ),
